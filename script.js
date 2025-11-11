a5e525b66a93e8821fa9903f017bce7632952c4b
Sam50506 <samhere5202@gmail.com>
2025-11-11 23:59:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API_KEY = "AIzaSyAf8yQTiE8jsTQIX3Gl6Y_UjUpK7ZVBzX0";
-const endpoint = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_ENDPOINT =
+  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 const ideaInput = document.getElementById("ideaInput");
 const generateBtn = document.getElementById("generateBtn");
 const clearBtn = document.getElementById("clearBtn");
