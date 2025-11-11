203c317c4e298d7ba8f40c1ad18caa63711c15cb
Sam50506 <samhere5202@gmail.com>
2025-11-12 02:11:31 +0530

    Delete api.js

--- api.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  return res.status(200).json({ message: "Serverless API is now working ✅" });
-}
